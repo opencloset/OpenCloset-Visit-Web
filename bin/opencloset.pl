714313bad5bbb29272828e1fe0052bc2bfd5a8d3
Keedi Kim <keedi.k@gmail.com>
2016-01-06 16:16:37 +0900

    Remove /donation controller & resource

--- bin/opencloset.pl
+++ bin/opencloset.pl
@@ -2902,66 +2902,6 @@ get '/sms' => sub {
     );
 };
 
-get '/donation' => sub {
-    my $self = shift;
-
-    #
-    # fetch params
-    #
-    my $p = $self->param('p') || 1;
-    my $s = $self->param('s') || app->config->{entries_per_page};
-    my $q = $self->param('q');
-
-    my $cond;
-    my $join = [ { 'user' => 'user_info' } ];
-    if ($q) {
-        $cond = [
-            { 'name'               => { like => "%$q%" } },
-            { 'email'              => { like => "%$q%" } },
-            { 'user_info.phone'    => { like => "%$q%" } },
-            { 'user_info.address4' => { like => "%$q%" } }, # 상세주소만 검색
-            { 'user_info.birth'    => { like => "%$q%" } },
-            { 'user_info.gender'   => $q },
-        ];
-
-        if ( $q =~ m/^\w{4,5}$/ ) {
-            push @$cond, { 'clothes.code' => sprintf( '%05s', uc($q) ) };
-            $join = [ 'clothes', { 'user' => 'user_info' } ];
-        }
-    }
-    else {
-        $cond = {};
-    }
-
-    my $rs = $DB->resultset('Donation')->search(
-        $cond,
-        {
-            join     => $join,
-            order_by => { -asc => 'id' },
-            page     => $p,
-            rows     => $s
-        },
-    );
-
-    my $bucket = $DB->resultset('Clothes')->search({
-        donation_id => undef
-    });
-
-    my $pageset = Data::Pageset->new({
-        total_entries    => $rs->pager->total_entries,
-        entries_per_page => $rs->pager->entries_per_page,
-        pages_per_set    => 5,
-        current_page     => $p,
-    });
-
-    $self->stash(
-        donation_list => $rs,
-        bucket        => $bucket,
-        pageset       => $pageset,
-        q             => $q || q{},
-    );
-};
-
 get '/stat/bestfit' => sub {
     my $self = shift;
 
